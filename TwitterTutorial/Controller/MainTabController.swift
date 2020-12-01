bcced768482f66f7ada86d34856a489936c81ec0
jc1cw <jeongchae1@naver.com>
2020-12-01 16:49:26 +0900

    Setup Navigation Bars Setting up Sticky Action Button Programmatically

--- TwitterTutorial/Controller/MainTabController.swift
+++ TwitterTutorial/Controller/MainTabController.swift
@@ -11,15 +11,33 @@ class MainTabController: UITabBarController {
     
     //MARK: - Properties
     
+    let actionButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.tintColor = .white
+        button.backgroundColor = .blue
+        button.setImage(UIImage(named: "new_tweet"), for: .normal)
+        return button
+    }()
+
     //MARK: - LifeCycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         configureViewControllers()
+        configureUI()
     }
     
     //MARK: Helpers
+    
+    func configureUI() {
+        view.addSubview(actionButton)
+        actionButton.anchor(bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.rightAnchor,
+                            paddingBottom: 64, paddingRight: 16, width: 56, height: 56)
+        actionButton.layer.cornerRadius = 56 / 2
+        
+        
+    }
     
     func configureViewControllers() {
         
@@ -30,10 +48,10 @@ class MainTabController: UITabBarController {
         let nav2 = templateNavigationController(image: UIImage(named: "search_unselected"), rootViewController: explore)
         
         let notifications = NotificationsController()
-        let nav3 = templateNavigationController(image: UIImage(named: "search_unselected"), rootViewController: notifications)
+        let nav3 = templateNavigationController(image: UIImage(named: "like_unselected"), rootViewController: notifications)
         
         let conversations = ConversationsController()
-        let nav4 = templateNavigationController(image: UIImage(named: "search_unselected"), rootViewController: conversations)
+        let nav4 = templateNavigationController(image: UIImage(named: "ic_mail_outline_white_2x-1"), rootViewController: conversations)
         
         viewControllers = [nav1, nav2, nav3, nav4]
     }
--- TwitterTutorial/Controller/NotificationsController.swift
+++ TwitterTutorial/Controller/NotificationsController.swift
@@ -15,10 +15,13 @@ class NotificationsController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .systemPurple
+        configureUI()
     }
     
     //MARK: - Helpers
     
+    func configureUI() {
+        view.backgroundColor = .white
+        navigationItem.title = "Notifications"
+    }
 }
