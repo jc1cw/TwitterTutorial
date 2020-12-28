c933bb571442bda4bf429049aaade0c94fc31f8d
jc1cw <jeongchae1@naver.com>
2020-12-28 17:25:33 +0900

    ~ Section12. Notifications 80

---
+++ TwitterTutorial/API/NotificationService.swift
@@ -0,0 +1,29 @@
+//
+//  NotificationService.swift
+//  TwitterTutorial
+//
+//  Created by 정채원 on 2020/12/28.
+//
+
+import Firebase
+
+struct NotificationService {
+    static let shared = NotificationService()
+    
+    func uploadNotification(type: NotificationType,
+                            tweet: Tweet? = nil,
+                            user: User? = nil) {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        
+        var values: [String: Any] = ["timestamp": Int(NSDate().timeIntervalSince1970),
+                                     "uid": uid,
+                                     "type": type.rawValue]
+        
+        if let tweet = tweet {
+            values["tweetID"] = tweet.tweetID
+            REF_NOTIFICATIONS.child(tweet.user.uid).childByAutoId().updateChildValues(values)
+        } else if let user = user {
+            REF_NOTIFICATIONS.child(user.uid).childByAutoId().updateChildValues(values)
+        }
+    }
+}
--- TwitterTutorial/Controller/FeedController.swift
+++ TwitterTutorial/Controller/FeedController.swift
@@ -41,6 +41,17 @@ class FeedController: UICollectionViewController {
     func fetchTweets() {
         TweetService.shared.fetchTweets { tweets in
             self.tweets = tweets
+            self.checkIfUserLikedTweets(self.tweets)
+        }
+    }
+            
+            func checkIfUserLikedTweets(_ tweets: [Tweet]) {
+                for (index, tweet) in tweets.enumerated() {
+                    TweetService.shared.checkIfUserLikedTweet(tweet) { didLike in
+                        guard didLike == true else { return }
+                        
+                        self.tweets[index].didLike = true
+            }
         }
     }
     
@@ -108,6 +119,20 @@ extension FeedController: UICollectionViewDelegateFlowLayout {
 // MARK: - TweetCellDelegate
 
 extension FeedController: TweetCellDelegate {
+    func handleLikeTapped(_ cell: TweetCell) {
+        guard let tweet = cell.tweet else { return }
+        
+        TweetService.shared.likeTweet(tweet: tweet) { (err, ref) in
+            cell.tweet?.didLike.toggle()
+            let likes = tweet.didLike ? tweet.likes - 1 : tweet.likes + 1
+            cell.tweet?.likes = likes
+            
+            //only upload niti if tweet is being liked
+            guard !tweet.didLike else { return }
+            NotificationService.shared.uploadNotification(type: .like, tweet: tweet)
+        }
+    }
+    
     func handleReplyTapped(_ cell: TweetCell) {
         guard let tweet = cell.tweet else { return }
         let controller = UploadTweetController(user: tweet.user, config: .reply(tweet))
